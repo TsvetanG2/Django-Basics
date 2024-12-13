f779f66febc3a2d2b4b6db3a013cea480e9018c8
TsvetanG2 <t.gerginov@yahoo.com>
2024-12-13 14:57:23 +0200

    SoftUni Django Exam 13th December - Django Basics

---
+++ TravelersHubApplication/traveler/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Traveler
+
+class TravelerForm(forms.ModelForm):
+    class Meta:
+        model = Traveler
+        fields = ['nickname', 'email', 'country', 'about_me']
---
+++ TravelersHubApplication/trip/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Trip
+
+class TripForm(forms.ModelForm):
+    class Meta:
+        model = Trip
+        fields = ['destination', 'summary', 'start_date', 'duration', 'image_url']
